0468e638571e68c467881852a955ff4cb53b1002
Alexandre Leuck <aleuck@inf.ufrgs.br>
2017-05-10 09:50:24 -0300

    beginning with sockets

--- dropboxClient.c
+++ dropboxClient.c
@@ -1,7 +1,32 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 #include "dropboxUtil.h"
 #include "dropboxClient.h"
 
 int main(int argc, char* argv[]) {
-	return 0;
+    struct sockaddr_in serv_addr;
+    int sock;
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <host> <port>\n", argv[0]);
+        exit(-1);
+    }
+
+    // create TCP socket
+	sock = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+    if (sock < 0) {
+        fprintf(stderr, "Error: Could not create socket.\n");
+        exit(1);
+    }
+
+    // Initialize
+    memset((char *) &serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(atoi(argv[2]));
+    exit(0);
 }
--- dropboxServer.c
+++ dropboxServer.c
@@ -1,7 +1,30 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
 #include "dropboxUtil.h"
 #include "dropboxServer.h"
 
 int main(int argc, char* argv[]) {
-	return 0;
+    struct sockaddr_in serv_addr, cli_addr;
+    int sock;
+    
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        exit(-1);
+    }
+
+    // create TCP socket
+	sock = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+    if (sock < 0) {
+        fprintf(stderr, "Error: Could not create socket.\n");
+        exit(1);
+    }
+
+    // Initialize
+    memset((char *) &serv_addr, 0, sizeof(serv_addr));
+    memset((char *) &cli_addr, 0, sizeof(cli_addr));
+	exit(0);
 }
