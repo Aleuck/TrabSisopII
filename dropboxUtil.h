964b7fbd69533191ca3ffd07119dc56ef0045765
Alexandre Leuck <aleuck@inf.ufrgs.br>
2017-06-10 11:26:22 -0300

    Added struct file_info

--- dropboxUtil.h
+++ dropboxUtil.h
@@ -1,9 +1,18 @@
 #ifndef __DROPBOX_UTIL
 #define __DROPBOX_UTIL
 
-
+// Maximum file name with null terminator
 #define MAXNAME 100
 
+#define REQUEST_LIST      1
+#define REQUEST_UPLOAD    2
+#define REQUEST_DOWNLOAD  3
 
+typedef struct file_info {
+  char name[MAXNAME];
+  char extension[MAXNAME];
+  char last_modified[MAXNAME];
+  int size;
+} FILE_INFO;
 
 #endif
