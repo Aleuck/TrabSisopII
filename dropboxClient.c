63adc9fbade7f27e8c99bcd58235de0ce89ff111
Alexandre Leuck <aleuck@inf.ufrgs.br>
2017-06-10 19:55:13 -0300

    login & connect refactor2

--- dropboxClient.c
+++ dropboxClient.c
@@ -45,61 +45,71 @@ int connect(SESSION *user_session, const char *host, const char *port) {
   user_session->server.sin_addr.s_addr = inet_addr(host);
   user_session->server.sin_family = AF_INET;
   user_session->server.sin_port = htons(atoi(port));
-  return 1;
 
   if (connect(user_session.connection, (struct sockaddr *) &user_session->server, sizeof (user_session->server)) < 0) {
-    printf("Failed to connect to server\n");
+    printf("Failed to connect to server.\n");
     return -1;
   }
+  return 1;
 }
 
-int login(SESSION user_session) {
+void disconnect(SESSION *user_session) {
+  close(user_session->connection);
+};
+
+int login(SESSION *user_session) {
+  uint16_t server_response_int;
+  send(user_session->connection, user_session->userid, MAXNAME, 0);
+  if(recv(user_session->connection, &server_response_int, sizeof(uint16_t), 0) == 0){
+    printf("Server cap reached\n");
+    return 0;
+  }
+  server_response_int = ntohs(server_response_int);
+  if(server_response_int == -1){
+    printf("too many connections\n");
+    return 0;
+  }
   return 1;
 }
 
 int main(int argc, char* argv[]) {
   struct stat st = {0};
-  struct sockaddr_in server_addr;
-  int client_sock, server_response_int, client_request = 1;
+  int server_response_int, client_request = 1;
   char response_buffer[MSG_SIZE], input_buffer[MSG_SIZE], *user_name;
+  pthread_t thread_cli, thread_sync;
   SESSION user_session = {0};
 
   if (argc < 4) {
     fprintf(stderr, "Usage: %s <fulano> <host> <port>\n", argv[0]);
     exit(-1);
   }
+
   strncpy(user_session.userid, argv[1], MAXNAME-1);
   if (!connect(&user_session, argv[2], argv[3])) {
     exit(1);
   }
 
+  if (!login(&user_session)) {
+    exit(1);
+  }
 
-  //
-  send(user_session.connection, user_session.userid, MAXNAME, 0);
-  if(recv(user_session.connection, &server_response_int, sizeof(int), 0) == 0){
-    printf("server cap reached\n");
-    return 0;
-  }
-  if(server_response_int == -1){
-    printf("too many connections\n");
-    return 0;
-  }
+  pthread_mutex_init(&user_session.connection_mutex);
 
   printf("Connected successfully to server: %d\n", server_response_int);
-  create_dir_for(argv[1]);
+  create_dir_for(user_session.userid);
 
-  while (client_request != 100) {
-
-    scanf("%d", &client_request);
-
-    switch (client_request) {
-      default:
-        send(client_sock, &client_request, sizeof(int), 0);
-        recv(client_sock, &server_response_int, sizeof(int), 0);
-        printf("server_response %d\n", server_response_int);
-        break;
-    }
-  }
-  close(client_sock);
+  // while (client_request != 100) {
+  //
+  //   scanf("%d", &client_request);
+  //
+  //   switch (client_request) {
+  //     default:
+  //       send(user_session.connection, &client_request, sizeof(int), 0);
+  //       recv(user_session.connection, &server_response_int, sizeof(int), 0);
+  //       printf("server_response %d\n", server_response_int);
+  //       break;
+  //   }
+  //}
+  disconnect(&user_session);
   exit(0);
 }
