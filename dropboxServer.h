e2f5151b199b7ba2f3e3ad30e2b29ef28afa755d
Luís Möllmann <luisgustavo181@gmail.com>
2017-06-11 00:49:38 -0300

    Fix overwrite on merge.

--- dropboxServer.h
+++ dropboxServer.h
@@ -1,9 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef DROPBOX_SERVER_H
+#define DROPBOX_SERVER_H
+
 #include "dropboxUtil.h"
 
-struct Client{
+#define MAX_LOGIN_COUNT 2
+#define MAXFILES 100
+#define MAX_CLIENTS 10
+
+struct client {
   int devices[2];
   char userid[MAXNAME];
+  struct file_info files[MAXFILES];
   int logged_in;
 };
+
+#endif /* ifndef DROPBOX_SERVER_H */
